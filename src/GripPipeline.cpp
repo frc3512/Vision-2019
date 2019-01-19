82bd19a0024bf7e5e9c01f98cd6058c3e03558e0
William Jin <wjin0702@yahoo.com>
2019-01-19 09:03:19 -0800

    Added a cameraserver

--- src/GripPipeline.cpp
+++ src/GripPipeline.cpp
@@ -11,23 +11,10 @@ void GripPipeline::Process(cv::Mat& source0){
 	//Step HSV_Threshold0:
 	//input
 	cv::Mat hsvThresholdInput = source0;
-	double hsvThresholdHue[] = {0.0, 180.0};
-	double hsvThresholdSaturation[] = {0.0, 62.342954159592516};
-	double hsvThresholdValue[] = {199.50539568345323, 255.0};
+	double hsvThresholdHue[] = {76.07913669064747, 112.76740237691};
+	double hsvThresholdSaturation[] = {91.72661870503596, 255.0};
+	double hsvThresholdValue[] = {121.53776978417267, 246.34125636672326};
 	hsvThreshold(hsvThresholdInput, hsvThresholdHue, hsvThresholdSaturation, hsvThresholdValue, this->hsvThresholdOutput);
-	//Step Blur0:
-	//input
-	cv::Mat blurInput = hsvThresholdOutput;
-	BlurType blurType = BlurType::MEDIAN;
-	double blurRadius = 7.207207207207207;  // default Double
-	blur(blurInput, blurType, blurRadius, this->blurOutput);
-	//Step Find_Blobs0:
-	//input
-	cv::Mat findBlobsInput = blurOutput;
-	double findBlobsMinArea = 1;  // default Double
-	double findBlobsCircularity[] = {0.0, 1.0};
-	bool findBlobsDarkBlobs = false;  // default Boolean
-	findBlobs(findBlobsInput, findBlobsMinArea, findBlobsCircularity, findBlobsDarkBlobs, this->findBlobsOutput);
 }
 
 /**
@@ -36,20 +23,6 @@ void GripPipeline::Process(cv::Mat& source0){
  */
 cv::Mat* GripPipeline::GetHsvThresholdOutput(){
 	return &(this->hsvThresholdOutput);
-}
-/**
- * This method is a generated getter for the output of a Blur.
- * @return Mat output from Blur.
- */
-cv::Mat* GripPipeline::GetBlurOutput(){
-	return &(this->blurOutput);
-}
-/**
- * This method is a generated getter for the output of a Find_Blobs.
- * @return BlobsReport output from Find_Blobs.
- */
-std::vector<cv::KeyPoint>* GripPipeline::GetFindBlobsOutput(){
-	return &(this->findBlobsOutput);
 }
 	/**
 	 * Segment an image based on hue, saturation, and value ranges.
@@ -63,64 +36,6 @@ std::vector<cv::KeyPoint>* GripPipeline::GetFindBlobsOutput(){
 	void GripPipeline::hsvThreshold(cv::Mat &input, double hue[], double sat[], double val[], cv::Mat &out) {
 		cv::cvtColor(input, out, cv::COLOR_BGR2HSV);
 		cv::inRange(out,cv::Scalar(hue[0], sat[0], val[0]), cv::Scalar(hue[1], sat[1], val[1]), out);
-	}
-
-	/**
-	 * Softens an image using one of several filters.
-	 *
-	 * @param input The image on which to perform the blur.
-	 * @param type The blurType to perform.
-	 * @param doubleRadius The radius for the blur.
-	 * @param output The image in which to store the output.
-	 */
-	void GripPipeline::blur(cv::Mat &input, BlurType &type, double doubleRadius, cv::Mat &output) {
-		int radius = (int)(doubleRadius + 0.5);
-		int kernelSize;
-		switch(type) {
-			case BOX:
-				kernelSize = 2 * radius + 1;
-				cv::blur(input,output,cv::Size(kernelSize, kernelSize));
-				break;
-			case GAUSSIAN:
-				kernelSize = 6 * radius + 1;
-				cv::GaussianBlur(input, output, cv::Size(kernelSize, kernelSize), radius);
-				break;
-			case MEDIAN:
-				kernelSize = 2 * radius + 1;
-				cv::medianBlur(input, output, kernelSize);
-				break;
-			case BILATERAL:
-				cv::bilateralFilter(input, output, -1, radius, radius);
-				break;
-        }
-	}
-	/**
-	 * Detects groups of pixels in an image.
-	 *
-	 * @param input The image on which to perform the find blobs.
-	 * @param minArea The minimum size of a blob that will be found.
-	 * @param circularity The minimum and maximum circularity of blobs that will be found.
-	 * @param darkBlobs The boolean that determines if light or dark blobs are found.
-	 * @param blobList The output where the MatOfKeyPoint is stored.
-	 */
-	//void findBlobs(Mat *input, double *minArea, double circularity[2],
-		//bool *darkBlobs, vector<KeyPoint> *blobList) {
-	void GripPipeline::findBlobs(cv::Mat &input, double minArea, double circularity[], bool darkBlobs, std::vector<cv::KeyPoint> &blobList) {
-		blobList.clear();
-		cv::SimpleBlobDetector::Params params;
-		params.filterByColor = 1;
-		params.blobColor = (darkBlobs ? 0 : 255);
-		params.minThreshold = 10;
-		params.maxThreshold = 220;
-		params.filterByArea = true;
-		params.minArea = minArea;
-		params.filterByCircularity = true;
-		params.minCircularity = circularity[0];
-		params.maxCircularity = circularity[1];
-		params.filterByConvexity = false;
-		params.filterByInertia = false;
-		cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
-		detector->detect(input, blobList);
 	}
 
 
--- src/Main.cpp
+++ src/Main.cpp
@@ -3,9 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <cameraserver/CameraServer.h>
 
 #include "GripPipeline.h"
-#include "opencv2/opencv.hpp"
+#include "opencv4/opencv2/opencv.hpp"
 #include "UdpSocket.hpp"
 
 uint32_t GetIP(uint8_t byte0, uint8_t byte1, uint8_t byte2, uint8_t byte3){
@@ -13,31 +14,47 @@ uint32_t GetIP(uint8_t byte0, uint8_t byte1, uint8_t byte2, uint8_t byte3){
 }
 
 int main() {
-    GripPipeLine pipe;
-    cs::UsbCamera camera;
-    uint16_t port = 5001;
+    GripPipeline pipe;
+    cs::UsbCamera camera = CameraServer::GetInstance().StartAutomaticCapture(); // creates camera named "USB Camera 0"
+    cs::CvSink cvsink = CameraServer::GetInstance().GetVideo();
+
+    uint16_t socketPort = 5001;
     uint32_t ip = GetIP(10, 35, 12, 2);
     UdpSocket socket;
+    cv::Mat input;
 
-    socket.bind(port);
+    // changed camera to dark
+    camera.SetVideoMode(PixelFormat.kYUYV, 320, 240, 30)
+    camera.SetResolution(160, 120);
+    camera.SetFPS(15);
+    socket.bind(socketPort);
 
-    while (/*help me*/){
-        if (/*help me*/){
-            continue;
-        }
-        cv::Mat input = /*help me*/
+    // Camera internals
+    cvsink.GrabFrameNoTimeout(input); // blocks until frame is avaliable
+    double focalLengthx = input.cols;
+    double focalLengthy = input.rows;
+    cv::Point2d center = cv::Point2d(input.cols/2, input.rows/2);
+    cv::Mat cameraMatrix = (cv::Mat_<double>(3,3) << focalLengthx, 0, center.x, 0, focalLengthy, center.y, 0, 0, 1);
+    cv::Mat distortionMatrix = cv::Mat::zeros(4,1,cv::DataType<double>::type); // no distortion assumed
+
+    // Main loop
+    while (){
+        cvsink.GrabFrameNoTimeout(input); // blocks until frame is avaliable
         pipe.Process(input);
+        cv::Mat output = pipe.GetHsvThresholdOutput();
+
         std::vector<cv::KeyPoint> points = pipe.findBlobsOutput;
-        std::vector<cv::Point2f> image_points;
+        std::vector<cv::Point2f> imagePoints;
         for (int i = 0; i < (points.size()-1); i++){
             cv::Point2f 2Dpoint;
             cv::KeyPoint::convert(points[i], 2Dpoint);
-            image_points.push_back(2Dpoint);
+            imagePoints.push_back(2Dpoint);
         }
-        std::vector<cv::Point3f> model_points;
-        model_points.push_back(cv::Point3f(/*help me*/));
-        cv::solvePnP(/*help me*/);
-
+        std::vector<cv::Point3f> modelPoints;
+        modelPoints.push_back(cv::Point3f(/*help me*/));
+        cv::Mat rotationMatrix translationMatrix;
+        cv::solvePnPRansac(modelPoints, imagePoints, cameraMatrix, distortionMatrix, rotationMatrix, translationMatrix);
+        
     }
 
 
